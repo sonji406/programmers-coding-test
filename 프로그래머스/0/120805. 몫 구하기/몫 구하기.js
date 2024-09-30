c239d1a8ec062f2f479a94126ae19e71d5852977
sonji <133579214+sonji406@users.noreply.github.com>
2024-10-01 00:20:54 +0900

    [level 0] Title: 몫 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120805. 몫 구하기/몫 구하기.js
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.js
@@ -2,9 +2,12 @@ function solution(num1, num2) {
     const answer = Math.trunc(num1 / num2);
     return answer;
 }
-/* Math.trunc는 소수점 이하를 잘라냄
+
+/* 
+Math.trunc는 소수점 이하를 잘라냄
 비슷한 함수 Math.floor()는 음수에 대해 내림 처리
 Math.floor(5.3 / 2); >> 2
 Math.floor(-5.3 / 2); >> -3
 결론 : Math.trunc()는 소수점을 제거하고, Math.floor()는 내림을 적용하여 더 작은 값을 반환하여
-두 숫자를 나눈 정확한 몫을 가지려면 Math.trunc()가 더 적절한 듯 */
+두 숫자를 나눈 정확한 몫을 가지려면 Math.trunc()가 더 적절한 듯 
+*/

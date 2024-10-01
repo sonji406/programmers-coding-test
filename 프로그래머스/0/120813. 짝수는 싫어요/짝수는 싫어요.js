58baa88e750ec8c2ca4591ea62e068b24b3f8337
sonji <133579214+sonji406@users.noreply.github.com>
2024-10-01 20:52:56 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,11 +1,19 @@
+// function solution(n) {
+//     const answer = [];
+    
+//     for (i=1; n>=i; i++) {
+//         if (i%2 !== 0) {
+//             answer.push(i);
+//         }    
+//     }
+    
+//     return answer;
+// }
+
 function solution(n) {
     const answer = [];
     
-    for (i=1; n>=i; i++) {
-        if (i%2 !== 0) {
-            answer.push(i);
-        }    
-    }
+    for (i=1; n>=i; i+=2) answer.push(i);
     
     return answer;
 }

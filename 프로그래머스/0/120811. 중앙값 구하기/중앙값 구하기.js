2681dc2bbb603c09b521d45ba7088ecc7f26a5cb
sonji <133579214+sonji406@users.noreply.github.com>
2024-10-01 20:06:48 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,4 +1,12 @@
+// function solution(array) {
+//     const answer = [...array].sort((a, b) => a - b);
+//     return answer[Math.trunc(array.length/2)];
+// }
+
+// 가독성을 고려해서 수정하면,,
 function solution(array) {
-    const answer = [...array].sort((a, b) => a - b);
-    return answer[Math.trunc(array.length/2)];
+    const sortedArray = [...array].sort((a, b) => a - b);
+    const middleIndex = Math.trunc(sortedArray.length / 2);
+    
+    return sortedArray[middleIndex];
 }

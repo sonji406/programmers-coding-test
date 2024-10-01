3e89c55161660049c1d63ca3a6c13ad1768baecf
sonji <133579214+sonji406@users.noreply.github.com>
2024-10-01 20:47:07 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.30 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,26 +1,26 @@
 function solution(array) {
     let answer = 0;
-    let maxConut = 0;
+    let maxCount = 0;
+    let isMultiple = false;
     const objectNumber = {};
     
-    for (i=0; i<array.length; i++) {
-        
+    for (let i = 0; i < array.length; i++) {
         if (array[i] in objectNumber) {
             objectNumber[array[i]] += 1;
         } else {
             objectNumber[array[i]] = 1;
         }
-        
     }
     
     for (let key in objectNumber) {
-        if (objectNumber[key] > maxConut) {
-            maxConut = objectNumber[key];
+        if (objectNumber[key] > maxCount) {
+            maxCount = objectNumber[key];
             answer = Number(key);
-        } else if(objectNumber[key] === maxConut) {
-            answer = -1;
+            isMultiple = false;
+        } else if (objectNumber[key] === maxCount) {
+            isMultiple = true;
         }
     }
     
-    return answer;
+    return isMultiple ? -1 : answer;
 }

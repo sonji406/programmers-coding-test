39437688721e347d3b51a5a82d717ba83973fe43
sonji <133579214+sonji406@users.noreply.github.com>
2024-10-02 23:57:47 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -2,7 +2,7 @@ function solution(money) {
     const answer = [];
     
     answer.push(Math.trunc(money/5500));
-    answer.push(money-Math.trunc(money/5500)*5500);
+    answer.push(money % 5500);
 
     return answer;
 }

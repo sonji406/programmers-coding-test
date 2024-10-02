f06ae92ccd426efe45099b5b304aa351a4e14823
sonji <133579214+sonji406@users.noreply.github.com>
2024-10-02 20:51:01 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,5 +1,10 @@
-function solution(n) {
-    const answer = Math.ceil(n / 7);
+// function solution(n) {
+//     const answer = Math.ceil(n / 7);
     
+//     return answer;
+// }
+
+function solution(n) {
+    const answer = Math.floor(n / 7) + (n % 7 === 0 ? 0 : 1); 
     return answer;
 }

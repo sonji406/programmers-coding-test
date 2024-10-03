33d1a2a37e54e721279d52bb5d0768cbfd2a577e
sonji <133579214+sonji406@users.noreply.github.com>
2024-10-03 22:44:59 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,9 +1,20 @@
+// function solution(num_list) {
+//     const answer = [];
+//     const arr = [...num_list];
+    
+//     for (i=0; i<num_list.length; i++) {
+//         answer.push(arr.pop());
+//     }
+    
+//     return answer;
+// }
+
 function solution(num_list) {
     const answer = [];
     const arr = [...num_list];
     
-    for (i=0; i<num_list.length; i++) {
-        answer.push(arr.pop());
+    for (i=num_list.length - 1; i >= 0; i--) {
+        answer.push(num_list[i]);
     }
     
     return answer;

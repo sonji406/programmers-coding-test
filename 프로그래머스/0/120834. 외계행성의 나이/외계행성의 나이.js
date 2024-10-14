04624f6a83b0566b5a72708c828894a367d23d31
sonji <133579214+sonji406@users.noreply.github.com>
2024-10-14 23:32:59 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,11 +1,12 @@
 function solution(age) {
     let answer = '';
-    const ageArr = ['a','b','c','d','e','f','g','h','i','j'];
+    // const ageArr = ['a','b','c','d','e','f','g','h','i','j'];
+    const age962 = 'abcdefghij';
     
     age = String(age);
     
     for(i=0; i<age.length; i++) {
-        answer += ageArr[age[i]];
+        answer += age962[age[i]];
     }
     
     return answer;

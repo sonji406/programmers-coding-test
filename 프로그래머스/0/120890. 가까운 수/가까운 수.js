c95e13f745c68715c87861378bcf86a3c479dbba
sonji <133579214+sonji406@users.noreply.github.com>
2024-10-16 23:51:32 +0900

    [level 0] Title: 가까운 수, Time: 0.31 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -1,15 +1,25 @@
-function solution(array, n) {
-    let answer = array[0];
-    let gap = Math.abs(n - array[0]);
+// function solution(array, n) {
+//     let answer = array[0];
+//     let gap = Math.abs(n - array[0]);
     
-    for (let i = 1; i < array.length; i++) {
-        let currentGap = Math.abs(n - array[i]);
+//     for (let i = 1; i < array.length; i++) {
+//         let currentGap = Math.abs(n - array[i]);
         
-        if (currentGap < gap || (currentGap === gap && array[i] < answer)) {
-            gap = currentGap;
-            answer = array[i];
-        }
-    }
+//         if (currentGap < gap || (currentGap === gap && array[i] < answer)) {
+//             gap = currentGap;
+//             answer = array[i];
+//         }
+//     }
     
-    return answer;
+//     return answer;
+// }
+
+function solution(array, n) {
+    array.sort((a,b) => Math.abs(n - a) - Math.abs(n - b) || a - b);
+
+    return array[0];
 }
+
+
+// n과의 차이를 절대값으로 계산하여 오름차순 정렬
+// 차이가 같은 경우 작은 숫자가 먼저 오도록 정렬

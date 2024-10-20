16735e295238af70a42d165115329a3d384bec97
sonji <133579214+sonji406@users.noreply.github.com>
2024-10-21 00:17:51 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,13 +1,22 @@
+// function solution(s1, s2) {
+//     let answer = 0;
+    
+//     for(i=0; i<s1.length; i++) {
+//         for(j=0; j<s2.length; j++) {
+//             if (s1[i] === s2[j]) {
+//                 answer += 1;
+//             }
+//         }
+//     }
+    
+//     return answer;
+// }
+
+
 function solution(s1, s2) {
     let answer = 0;
+    const arr = s1.concat(s2);
+    const set = [...new Set(arr)];
     
-    for(i=0; i<s1.length; i++) {
-        for(j=0; j<s2.length; j++) {
-            if (s1[i] === s2[j]) {
-                answer += 1;
-            }
-        }
-    }
-    
-    return answer;
+    return arr.length-set.length;
 }

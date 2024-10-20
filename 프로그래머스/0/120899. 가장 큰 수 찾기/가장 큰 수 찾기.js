057b407009831b03be87f00dac323157dea746d2
sonji <133579214+sonji406@users.noreply.github.com>
2024-10-20 22:39:58 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,6 +1,20 @@
-function solution(array) {
-    const maxNumber = [...array].sort((a, b) => a - b).pop();
-    const answer = [maxNumber, array.indexOf(maxNumber)];
+// function solution(array) {
+//     const maxNumber = [...array].sort((a, b) => a - b).pop();
+//     const answer = [maxNumber, array.indexOf(maxNumber)];
 
-    return answer;
+//     return answer;
+// }
+
+function solution(array) {
+    let maxNumber = array[0];
+    let maxIndex = 0;
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > maxNumber) {
+            maxNumber = array[i];
+            maxIndex = i;
+        }
+    }
+
+    return [maxNumber, maxIndex];
 }

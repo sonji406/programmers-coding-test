1b6eb3a57a6b4e92d07f4f6c3610a67445a0a270
sonji <133579214+sonji406@users.noreply.github.com>
2024-10-21 00:54:12 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -1,11 +1,17 @@
-function solution(n, numlist) {
-    let answer = [];
+// function solution(n, numlist) {
+//     let answer = [];
     
-    for(i=0;i<numlist.length; i++) {
-        if (numlist[i]%n === 0) {
-            answer.push(numlist[i]);
-        }
-    }
+//     for(i=0;i<numlist.length; i++) {
+//         if (numlist[i]%n === 0) {
+//             answer.push(numlist[i]);
+//         }
+//     }
+    
+//     return answer;
+// }
+
+function solution(n, numlist) {
+    let answer = numlist.filter((num) => num%n===0);
     
     return answer;
 }

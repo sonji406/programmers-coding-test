bd8ea7b070cb095ae4753996b30e474e30247afe
sonji <133579214+sonji406@users.noreply.github.com>
2024-10-21 23:02:22 +0900

    [Bronze V] Title: X보다 작은 수, Time: 112 ms, Memory: 11004 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
@@ -1,19 +1,20 @@
-const readline = require('readline');
-
+const readline = require('readline'); // Node.js의 내장 모듈 readline
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+    input: process.stdin, // 콘솔에서 입력받는 스트림
+    output: process.stdout, // 콘솔에 출력하는 스트림
 });
 
 let input = [];
 
 rl.on('line', function (line) {
+    // 'line' 이벤트 리스너 -> 사용자가 엔터 키를 눌러 한 줄의 입력을 완료할 때마다 발생
     input.push(line);
 }).on('close', function () {
-    const [N, X] = input[0].split(' ').map(Number);
-    const numbers = input[1].split(' ').map(Number);
-    
-    const result = numbers.filter(num => num < X);
-    
+    // 코드 작성
+    const [N, X] = input[0].split(' ');
+    const result = input[1]
+        .split(' ')
+        .filter((number) => Number(number) < Number(X));
+
     console.log(result.join(' '));
 });

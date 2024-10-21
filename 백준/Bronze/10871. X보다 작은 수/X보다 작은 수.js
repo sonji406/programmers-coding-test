82ab6748a5600284e9eebe89ebda6c722818e6ce
sonji <133579214+sonji406@users.noreply.github.com>
2024-10-21 23:12:55 +0900

    불필요한 중복 형 변환 수정
    불필요한 중복 형 변환 수정

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
@@ -10,11 +10,9 @@ rl.on('line', function (line) {
     // 'line' 이벤트 리스너 -> 사용자가 엔터 키를 눌러 한 줄의 입력을 완료할 때마다 발생
     input.push(line);
 }).on('close', function () {
-    // 코드 작성
-    const [N, X] = input[0].split(' ');
-    const result = input[1]
-        .split(' ')
-        .filter((number) => Number(number) < Number(X));
+    const [N, X] = input[0].split(' ').map(Number);
+    const numbers = input[1].split(' ').map(Number);
+    const result = numbers.filter((number) => number < X);
 
     console.log(result.join(' '));
 });

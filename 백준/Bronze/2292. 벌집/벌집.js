c4077fb35c796821649da266900e85e0220501f8
sonji <133579214+sonji406@users.noreply.github.com>
2024-10-24 08:58:43 +0900

    [Bronze II] Title: 벌집, Time: 116 ms, Memory: 10172 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.js
@@ -0,0 +1,23 @@
+const readline = require('readline'); // Node.js의 내장 모듈 readline
+const rl = readline.createInterface({
+    input: process.stdin, // 콘솔에서 입력받는 스트림
+    output: process.stdout, // 콘솔에 출력하는 스트림
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    // 'line' 이벤트 리스너 -> 사용자가 엔터 키를 눌러 한 줄의 입력을 완료할 때마다 발생
+    input.push(line);
+}).on('close', function () {
+    const n = input[0].split(' ').map(Number);
+    let roomNumber = 1;
+    let count = 1;
+
+    while (n > roomNumber) {
+        roomNumber += 6 * count;
+        count++;
+    }
+
+    console.log(count);
+});
